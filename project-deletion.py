0bb78b4f5b074898867473cca3629ef4cdf9f834
SandeepPochu-test <63260565+SandeepPochu-test@users.noreply.github.com>
2020-04-07 12:46:52 -0700

    Update project-deletion.py

--- project-deletion.py
+++ project-deletion.py
@@ -7,7 +7,7 @@ service = discovery.build('cloudresourcemanager', 'v1', credentials=credentials)
 
 # The Project ID (for example, `foo-bar-123`).
 # Required.
-project_id = 'test-273120'  # TODO: Update placeholder value.
+project_id = 'project_id'  # TODO: Update placeholder value.
 
 request = service.projects().delete(projectId=project_id)
 request.execute()
